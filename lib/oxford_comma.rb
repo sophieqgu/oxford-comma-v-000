e96f37e98c8001ad86a335329e5931e7a0f481ab
Sophie Gu <sophieqgu@gmail.com>
2018-10-08 02:43:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.size
+    when 1
+    array.join 
+    when 2 
+    array.join(" and ")
+    else 
+      last = array.pop()
+      string = array.join ", "
+      string << ", and #{last}"
+  end 
 end
