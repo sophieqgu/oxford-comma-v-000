dc33735baef183ec6a4fcf6e4eca770b4c538566
Sophie Gu <sophieqgu@gmail.com>
2018-10-08 02:42:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case array.size
+    when 1
+    array.join 
+    when 2 
+    array.join(" and ")
+    else 
+      last = array.pop()
+      puts array 
+      string = array.join ", "
+      string << ", and #{last}"
+  end 
 end
